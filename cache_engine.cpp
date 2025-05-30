2e51b4b7f01da9630854a69e141012b516ee7349
Alex <sanches0796@gmail.com>
2025-05-30 15:26:31 +0300

    working

--- cache_engine.cpp
+++ cache_engine.cpp
@@ -1,82 +1,75 @@
-class Cache_Line
-{
+class Cache_Line {
 
-  bool* ways;
-  int* tags;
-  int tag_size;
+    bool *ways;
+    int *tags;
+    int tag_size;
+
   public:
-    //Constrcutors
+    // Constructors
     Cache_Line(int tag_size);
 
-    //functions
+    // functions
     bool read_to_cline(int address);
     bool write_to_cline(int address);
 
-    //r 0x000 00001
-    // ways[0]=true;
-    //w 0x000 00003
-    // ways[0] true
+    // r 0x000 00001
+    //  ways[0]=true;
+    // w 0x000 00003
+    //  ways[0] true
 };
 
-Cache_Line::Cache_Line(int tag_size)
-{
+Cache_Line::Cache_Line(int tag_size) {}
+bool Cache_Line::read_to_cline(int address) {}
+bool Cache_Line::write_to_cline(int address) {}
 
-}
-bool Cache_Line::read_to_cline(int address)
-{
-
-}
-bool Cache_Line::write_to_cline(int address)
-{
-
-}
-
-class Cache_Engine
-{
+class Cache_Engine {
   private:
-    int cache_size; //in bytes
-    int block_size; //in bytes
-    int offset_size_b;  //number of bits
-    int num_of_sets;     //number of sets
-    int set_size_b; //set size of bits
-    int assoc_l1;  //1 way, 2 way ,etc'
-    int assoc_l2;  //1  way, 2 way ,etc'
-    int cyc_acc_L1;
-    int cyc_acc_L2;
-    int cyc_acc_mem;
-
+    // general config
+    int cyc_acc_mem;     // how much cycles takes to acces DRAM
+    int block_size_bits; // [bits] | represents log_2(actual_block_size)
+    int block_size;      // [bytes] | we will evaluate by pow(2,block_size_bits)
     bool is_write_alloc;
 
-    Cache_Line* L1_cache;
-    Cache_Line* L2_cache;
+    // L1 config
+    int l1_size_bits;     // [bits] | represents log_2(actual_L1_size_in_bytes)
+    int l1_assoc;         // 1 way, 2 way, etc.
+    int l1_num_of_sets;   // [#sets] | evaluate by: #block / #ways_of_L2
+    int l1_set_size_bits; // [bits] | evaluate by: log_2(l1_num_of_sets)
+    int cyc_acc_L1;       // how much cycles takes to acces L1
 
-    //SIM INFO
-  
+    // L2 config
+    int l2_size_bits;     // [bits] | represents log_2(actual_L2_size_in_bytes)
+    int l2_assoc;         // 1 way, 2 way, etc.
+    int l2_num_of_sets;   // [#sets] | evaluate by: #block / #ways_of_L2
+    int l2_set_size_bits; // [bits] | evaluate by: log_2(l1_num_of_sets)
+    int cyc_acc_L2;       // how much cycles takes to acces L2
+
+    Cache_Line *L1_cache;
+    Cache_Line *L2_cache;
+
+    // SIM INFO
+
   public:
-    //Constrcutors
-    Cache_Engine(int mem_cyc,int block_size,int l1_size,int l2_size,int l1_cyc,int l2_cyc,int l1_assoc,int l2_assoc,bool is_write_alloc);
+    // Constrcutors
+    Cache_Engine(int mem_cyc, int block_size, int l1_size, int l2_size,
+                 int l1_cyc, int l2_cyc, int l1_assoc, int l2_assoc,
+                 bool is_write_alloc);
 
     void write_to_mem(int address);
     void read_to_mem(int address);
     void print_DEBUG();
-     
-
 };
 
-Cache_Engine::Cache_Engine(int mem_cyc,int block_size,int l1_size,int l2_size,int l1_cyc,int l2_cyc,int l1_assoc,int l2_assoc,bool is_write_alloc)
-{
+Cache_Engine::Cache_Engine(int mem_cyc, int block_size, int l1_size,
+                           int l2_size, int l1_cyc, int l2_cyc, int l1_assoc,
+                           int l2_assoc, bool is_write_alloc) {
 
+    this;
 }
 
-void Cache_Engine::write_to_mem(int address)
-{
+void Cache_Engine::write_to_mem(int address) {}
+void Cache_Engine::read_to_mem(int address) {}
+void Cache_Engine::print_DEBUG() {}
 
-}
-void Cache_Engine::read_to_mem(int address)
-{
-
-}
-void Cache_Engine::print_DEBUG()
-{
-
-}
+// initializing
+Cache_Engine myCache;
