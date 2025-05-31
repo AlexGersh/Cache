0f1d44d2dfaab44e79e07aba673d54ac299d92d4
AlexGersh <sanches0796@gmail.com>
2025-05-31 14:49:57 +0300

    cache_engine in progress

--- cache_engine.cpp
+++ cache_engine.cpp
@@ -51,7 +51,7 @@ class Cache_Line {
     // line. else, in MISS will add block to the cache line. status 0 - HIT and
     // no repalce status 1 - HIT and replace no dirty bit status 2 - HIT and
     // replace with dirty bit status 3 - MISS
-    void write_to_cline(uint32_t tag, uint32_t offset, int *out, int *status);
+    void write_to_cline(uint32_t tag, int *out, int *status);
 
     void get_LRU();
 
@@ -79,8 +79,7 @@ Cache_Line::Cache_Line(int num_of_ways, bool is_write_alloc) {
 // Functions
 bool Cache_Line::read_from_cline(uint32_t tag, uint32_t offset) {}
 
-void Cache_Line::write_to_cline(uint32_t tag, uint32_t offset, int *out,
-                                int *status) {
+void Cache_Line::write_to_cline(uint32_t tag, int *out, int *status) {
 
     // searching for HIT
     for (int i = 0; i < this->num_of_ways; i++) {
@@ -124,16 +123,17 @@ void Cache_Line::print_DEBUG() {
 class Cache_Engine {
   private:
     // general config
-    int cyc_acc_mem;     // how much cycles takes to acces DRAM
-    int block_size_bits; // [bits] | represents log_2(actual_block_size)
-                         // also represents the offset_size_bits
-    int block_size;      // [bytes] | evaluated by pow(2,block_size_bits)
+    int cyc_acc_mem;            // how much cycles takes to acces DRAM
+    int block_offset_size_bits; // [bits] | represents log_2(actual_block_size)
+                                // also represents the offset_size_bits
+    int block_size; // [bytes] | evaluated by pow(2,block_offset_bits)
     bool is_write_alloc;
 
     // L1 config
     int l1_size_bits;     // [bits] | represents log_2(actual_L1_size_in_bytes)
-    int l1_assoc;         // 1 way, 2 way, etc.
-    int l1_num_of_sets;   // [#sets] | evaluate by: #block / #ways_of_L2
+    int l1_num_of_ways;   // number of ways at L1 level
+    int l1_num_of_blocks; // [blocks] | evaluate by l1_size / block_size
+    int l1_num_of_sets;   // [#sets] | evaluate by: #blocks_of_L1 / #ways_of_L1
     int l1_set_size_bits; // [bits] | evaluate by: log_2(l1_num_of_sets)
     int cyc_acc_L1;       // how much cycles takes to acces L1
     int l1_tag_size_bits; // [bits] | how much tag bits we have
@@ -141,10 +141,11 @@ class Cache_Engine {
 
     // L2 config
     int l2_size_bits;     // [bits] | represents log_2(actual_L2_size_in_bytes)
-    int l2_assoc;         // 1 way, 2 way, etc.
-    int l2_num_of_sets;   // [#sets] | evaluate by: #block / #ways_of_L2
-    int l2_set_size_bits; // [bits] | evaluate by: log_2(l1_num_of_sets)
-    int cyc_acc_L2;       // how much cycles takes to acces L2
+    int l2_num_of_ways;   // number of ways at L2 level
+    int l2_num_of_blocks; // [blocks] | evaluate by l2_size / block_size
+    int l2_num_of_sets;   // [#sets] | evaluate by: #blocks_of_L2 / #ways_of_L2
+    int l2_set_size_bits; // [bits] | evaluate by: log_2(l2_num_of_sets)
+    int cyc_acc_L2;       // how much cycles takes to access L2
     int l2_tag_size_bits; // [bits] | how much tag bits we have
                           // evaluated by function
 
@@ -162,10 +163,27 @@ class Cache_Engine {
                  bool is_write_alloc);
 
     // Functions
-    void write_to_mem(uint32_t address);
-    void read_from_mem(uint32_t address);
-    void print_DEBUG();
+
+    // gets address and number of set bits
+    // returns set bits of the address
+    uint32_t getSet(uint32_t address, int set_size_bits);
+
+    // returns tag size in [bits]
     int evaluate_tag_size(int offset_size_bits, int set_size_bits);
+
+    /* param @address - keeps the addrss of the instruction
+     * param @for_cache_L1 - if true: we want the tag for L1, false - for L2
+     * returns tag bits of the address */
+    uint32_t getTag(uint32_t address, bool for_cache_L1);
+
+    // write to cache
+    void write_to_mem(uint32_t address);
+
+    // read from cache
+    void read_from_mem(uint32_t address);
+
+    // prints cache_Engine for debugging
+    void print_DEBUG();
 
     // Destructors
     ~Cache_Engine();
@@ -180,27 +198,30 @@ Cache_Engine::Cache_Engine(int mem_cyc, int block_size, int l1_size,
                            int l2_assoc, bool is_write_alloc) {
     // General
     this->cyc_acc_mem = mem_cyc;
-    this->block_size_bits = block_size;
-    this->block_size = std::pow(2, this->block_size_bits); // size in bytes
+    this->block_offset_size_bits = block_size; //
+    this->block_size =
+        std::pow(2, this->block_offset_size_bits); // size in bytes
     this->is_write_alloc = is_write_alloc;
 
     // L1
     this->l1_size_bits = l1_size;
-    this->l1_assoc = l1_assoc;
-    this->l1_num_of_sets = pow(2, this->l1_size_bits) / this->l1_assoc;
+    this->l1_num_of_ways = std::pow(2, l1_assoc);
+    this->l1_num_of_blocks = std::pow(2, l1_size_bits) / this->block_size;
+    this->l1_num_of_sets = l1_num_of_blocks / l1_num_of_ways;
     this->l1_set_size_bits = std::log2(l1_num_of_sets);
     this->cyc_acc_L1 = l1_cyc;
     this->l1_tag_size_bits =
-        evaluate_tag_size(this->block_size_bits, this->l1_set_size_bits);
+        evaluate_tag_size(this->block_offset_size_bits, this->l1_set_size_bits);
 
     // L2
     this->l2_size_bits = l2_size;
-    this->l2_assoc = l2_assoc;
-    this->l2_num_of_sets = pow(2, this->l2_size_bits) / this->l2_assoc;
+    this->l2_num_of_ways = std::pow(2, l2_assoc);
+    this->l2_num_of_blocks = std::pow(2, l2_size_bits) / this->block_size;
+    this->l1_num_of_sets = l2_num_of_blocks / l2_num_of_ways;
     this->l2_set_size_bits = std::log2(l2_num_of_sets);
     this->cyc_acc_L2 = l2_cyc;
     this->l2_tag_size_bits =
-        evaluate_tag_size(this->block_size_bits, this->l2_set_size_bits);
+        evaluate_tag_size(this->block_offset_size_bits, this->l2_set_size_bits);
 
     // Cache_Line arrays
     L1_cache = new Cache_Line[l1_num_of_sets];
@@ -229,36 +250,64 @@ Cache_Engine::~Cache_Engine() {
 }
 
 // functions
-void Cache_Engine::write_to_mem(uint32_t address) { // 0xFFFFFFFF >> 2 = 0x
-    /*
-    int out1;
-    int out2;
-    int status;
-    set_L1 = getSet(address);
-    cline_L1 = this->L1_cache[set];
-
-    cline_L1.write_to_cline(address,out,status);
-
-    if(staus == 0)
-      siyamnu
-    if(status == 1)
-      siyamnu;
-    if(status == 2)
-      cline_L2.write_to_cline(out1,out2,status);  //only modifing LRU
-      siyamnu;
-    if(status == 3)
-      cline_L2.write_to_cline(address,out2,status);
-      siyamnu;
-    */
+//
+uint32_t Cache_Engine::getSet(uint32_t address, int set_size_bits) {
+    uint32_t set_mask = (1U << set_size_bits) - 1;
+    uint32_t temp = address;
+    temp >>= this->block_offset_size_bits; // remove offset bits
+    return temp & set_mask;
 }
 
-void Cache_Engine::read_from_mem(uint32_t address) {}
-
-void Cache_Engine::print_DEBUG() {}
-
+//
 int Cache_Engine::evaluate_tag_size(int offset_size_bits, int set_size_bits) {
     return 32 - set_size_bits - offset_size_bits;
 }
+
+//
+uint32_t Cache_Engine::getTag(uint32_t address, bool for_cache_L1) {
+    int tag_size = this->l2_tag_size_bits; // default assume for cache L2
+    if (for_cache_L1) {                    // change to L1 if asked
+        tag_size = this->l1_tag_size_bits;
+    }
+
+    uint32_t tag_mask = (1U << tag_size) - 1;
+    uint32_t temp = address;
+    temp >>= (32 - tag_size); // remove offset adn set bits
+    return temp & tag_mask;
+}
+
+//
+void Cache_Engine::write_to_mem(uint32_t address) {
+
+    int out1;
+    int out2;
+    int status;
+    uint32_t set_L1 = getSet(address, this->l1_set_size_bits);
+    uint32_t set_L2 = getSet(address, this->l2_set_size_bits);
+    uint32_t tag_L1 = getTag(address, true);
+    uint32_t tag_L2 = getTag(address, false);
+    Cache_Line cline_L1 = this->L1_cache[set_L1];
+    Cache_Line cline_L2 = this->L2_cache[set_L2];
+
+    cline_L1.write_to_cline(tag_L1, &out1, &status);
+
+    // if(staus == 0)
+    //   siyamnu
+    // if(status == 1)
+    //   siyamnu;
+    // if(status == 2)
+    //   cline_L2.write_to_cline(out1,out2,status);  //only modifing LRU
+    //   siyamnu;
+    // if(status == 3)
+    //   cline_L2.write_to_cline(address,out2,status);
+    //   siyamnu;
+}
+
+//
+void Cache_Engine::read_from_mem(uint32_t address) {}
+
+//
+void Cache_Engine::print_DEBUG() {}
 
 // initializing
 Cache_Engine myCache;
